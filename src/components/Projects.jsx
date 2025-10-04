b74d4629f4f720cb2b4bb7ddb89d45d0cdbe992b
ces08 <christinesong08@gmail.com>
2025-10-04 19:35:46 -0400

    added about me"

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,17 @@ import { Link } from "react-router-dom";
 
 export default function Projects(){
     return(
+        <div className = "flex flex-col">
+        <section className = 'flex'>
+            <div className = 'title-container p-10 gap-5 '>
+                <div className = "text-8xl font-semibold text-[#7FD0E7] font-sans">
+                    Christine Song
+                </div>
+                <div className = 'text-4xl text-[#D0D0D1]'>
+                    STUDENT DEVELOPER
+                </div>
+            </div>
+        </section>
         <div className = "grid grid-cols-2 gap-10 m-10">
 
             {Object.entries(projects).map(([id, project])=>(
@@ -17,13 +28,14 @@ export default function Projects(){
             )) }
             
         </div>
+    </div>
     )
 }
 
 export function ProjectPreview({id, img_path, img_name, year, desc}){
     return(
         <>
-        <Link to = {`/projects/${id}`} className = "cor transform hover:scale-101 transition duration-200">
+        <Link to = {`/${id}`} className = "cor transform hover:scale-101 transition duration-200">
             <img src = {img_path} alt = {img_name} className = "rounded-2xl"/>
             <div className = " text-black dark:text-white text-2xl font-semibold  p-1 hover:text-accent">{img_name} <span className= " font-light">({year})</span></div>
              <div className="text-black dark:text-white">{desc}</div>
